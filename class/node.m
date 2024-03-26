b3d547680b99199d7d24fa6fa83bfc639d2b27c5
goof-off-fatty <yantong@sdnu.edu.cn>
2024-03-26 16:34:13 +0800

    <refactor>

--- class/node.m
+++ class/node.m
@@ -1,12 +1,29 @@
-classdef node < handle
+classdef Node < handle
 
     properties
+        nodeID=1;
         position=0;
         radius=200; % coverage, m
-        content=[];
+        content; % caching space
     end
 
     methods
+        function obj=Node(ID,position,radius,contentSize)
+            obj.nodeID=ID;
+            obj.position=position;
+            obj.radius=radius;
+            obj.content=cell(contentSize,1);
+        end
+
+        function obj=updateContent(obj,content)
+            % update storage with content
+            if length(obj.content)<length(content)
+                disp('Cannot update content due to storage overflow.');
+            else
+                obj.content=content;
+            end
+        end
+
         function sendSignal(obj,signal)
             communicationNetwork=getCommunicationNetwork();
             communicationNetwork.send(obj,signal);
